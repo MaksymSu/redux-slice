2acfb634ae3d46b4927fd39dce09c359f6234147
Maksym Sukhomlynov <lbstalker512@gmail.com>
2021-08-09 10:55:19 +0300

    counter ready, no init

--- src/index.js
+++ src/index.js
@@ -1,2 +1,32 @@
 import './styles.css'
 
+import {createStore, applyMiddleware} from "redux";
+import {INCREMENT} from "./redux/types";
+
+import {logger} from "redux-logger";
+import {rootReducer} from "./redux/rootReducer";
+import {decrement, increment} from "./redux/actions";
+
+
+const store = createStore(
+  rootReducer,
+  0,
+  applyMiddleware(logger)
+);
+
+
+
+
+document.getElementById('add').addEventListener('click',
+  () => store.dispatch(increment())
+);
+
+document.getElementById('sub').addEventListener('click',
+  () => store.dispatch(decrement())
+);
+
+function render() {
+ document.getElementById('counter').textContent = store.getState();
+}
+
+store.subscribe(render);
